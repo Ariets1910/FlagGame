063819d92e218dab49ae4f1738db08c097d5bcf5
Ariets1910 <Ariets1910@gmail.com>
2020-02-08 15:12:59 +0200

    Updates

---
+++ precache-manifest.4ba1745a53a33be2d3462c480209e70e.js
@@ -0,0 +1,226 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16c1624fe3d37a73b234d15390159c51",
+    "url": "/Test/index.html"
+  },
+  {
+    "revision": "b59826b4a83a3467600d",
+    "url": "/Test/static/css/main.be45f2ae.chunk.css"
+  },
+  {
+    "revision": "8cf354e7e2516ed34c26",
+    "url": "/Test/static/js/2.e56a2afc.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/Test/static/js/2.e56a2afc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b59826b4a83a3467600d",
+    "url": "/Test/static/js/main.d9dc51f8.chunk.js"
+  },
+  {
+    "revision": "0a0ca19a37a49c1498e2",
+    "url": "/Test/static/js/runtime-main.441ccc11.js"
+  },
+  {
+    "revision": "daeb5108dcabc9e00e731ee40e546b40",
+    "url": "/Test/static/media/Algeria.daeb5108.svg"
+  },
+  {
+    "revision": "b2c27af1f73d31f5215767ec5a7db833",
+    "url": "/Test/static/media/Angola.svg.b2c27af1.png"
+  },
+  {
+    "revision": "3e880c0908c4d0b84a3ed1c5d592155f",
+    "url": "/Test/static/media/Argentina.svg.3e880c09.png"
+  },
+  {
+    "revision": "5987ad8946470c03df345ff26d8447ae",
+    "url": "/Test/static/media/Bhutan.svg.5987ad89.png"
+  },
+  {
+    "revision": "0cf19ed199e441f518b0959dbaeea420",
+    "url": "/Test/static/media/Bolivia.svg.0cf19ed1.png"
+  },
+  {
+    "revision": "c03c4c48928779d7d7a31080b1f05fd3",
+    "url": "/Test/static/media/Brazil.svg.c03c4c48.png"
+  },
+  {
+    "revision": "2cfb3f9a92e436dfadba9f8541e3d72c",
+    "url": "/Test/static/media/Canada.svg.2cfb3f9a.webp"
+  },
+  {
+    "revision": "6f64b37ef6b55bfc62c4cd79400320a1",
+    "url": "/Test/static/media/China.6f64b37e.svg"
+  },
+  {
+    "revision": "b8f2dbf2050ad1937631d811e8017c67",
+    "url": "/Test/static/media/Croatia.svg.b8f2dbf2.png"
+  },
+  {
+    "revision": "5681392152557028b9a4f29abc97e80d",
+    "url": "/Test/static/media/Cuba.svg.56813921.png"
+  },
+  {
+    "revision": "35ccd81c1bf1528a0917af66b45fe980",
+    "url": "/Test/static/media/Egypt.svg.35ccd81c.png"
+  },
+  {
+    "revision": "b6787286211ae319ba98b01a4cafbc17",
+    "url": "/Test/static/media/Ethiopia.svg.b6787286.png"
+  },
+  {
+    "revision": "259e73008e6752adeb699d9fd62fb3e7",
+    "url": "/Test/static/media/Fiji.svg.259e7300.png"
+  },
+  {
+    "revision": "ba0d527792ca7f399aff67afdb331e84",
+    "url": "/Test/static/media/Germany.svg.ba0d5277.webp"
+  },
+  {
+    "revision": "e9b657b839dba5a312c5ea243cf43c3d",
+    "url": "/Test/static/media/Indonesia.e9b657b8.svg"
+  },
+  {
+    "revision": "c51e0dcec132e7d56d2274a4279fc023",
+    "url": "/Test/static/media/Iran.svg.c51e0dce.png"
+  },
+  {
+    "revision": "a24104f41837aa56c9b99a1e223b923e",
+    "url": "/Test/static/media/Japan.svg.a24104f4.png"
+  },
+  {
+    "revision": "303df07b8126e110084bdd5318b312b0",
+    "url": "/Test/static/media/Kazakhstan.svg.303df07b.png"
+  },
+  {
+    "revision": "ea818b9ba7c652fdc2d2b29a36d3dace",
+    "url": "/Test/static/media/Kiselevovpad.ea818b9b.jpg"
+  },
+  {
+    "revision": "d625a5aa05315f122cd675ceeabae17e",
+    "url": "/Test/static/media/Moldova.svg.d625a5aa.png"
+  },
+  {
+    "revision": "951e43e89e2f3fb602fa788b68e1725a",
+    "url": "/Test/static/media/Natalia.951e43e8.jpg"
+  },
+  {
+    "revision": "46ad54e7f2bab41570a0347158e7998b",
+    "url": "/Test/static/media/Nepal.svg.46ad54e7.png"
+  },
+  {
+    "revision": "485ebc320070799e7bbc3625e78dd8a7",
+    "url": "/Test/static/media/Niger.svg.485ebc32.png"
+  },
+  {
+    "revision": "80f81bcd70808fe11b4b22a5e018f874",
+    "url": "/Test/static/media/Niue.svg.80f81bcd.png"
+  },
+  {
+    "revision": "77ce8d9ca6319b7fc04b605f90846b4e",
+    "url": "/Test/static/media/North_Korea.svg.77ce8d9c.png"
+  },
+  {
+    "revision": "5968606cdab1a186174cc6ac9e2b9f15",
+    "url": "/Test/static/media/Pakistan.svg.5968606c.png"
+  },
+  {
+    "revision": "a13d2a2f6be3b01be4ccccafa5ffdb92",
+    "url": "/Test/static/media/Palau.svg.a13d2a2f.png"
+  },
+  {
+    "revision": "2f686faca6bb0b8a99d0f3ed1d043588",
+    "url": "/Test/static/media/Peru.svg.2f686fac.png"
+  },
+  {
+    "revision": "b9e8b6237d11696882f0d075f85aed57",
+    "url": "/Test/static/media/Portugal.svg.b9e8b623.png"
+  },
+  {
+    "revision": "0f2cf1ff297354e89c443d736971fd5d",
+    "url": "/Test/static/media/Rwanda.svg.0f2cf1ff.png"
+  },
+  {
+    "revision": "cc037991ca2b4d53fe093676b0ede06e",
+    "url": "/Test/static/media/Saba.svg.cc037991.png"
+  },
+  {
+    "revision": "b62df313096b2d0205e937926db159a5",
+    "url": "/Test/static/media/SouthKorea.b62df313.svg"
+  },
+  {
+    "revision": "d3fac95b2096cb9d7c68487b5c0c1a08",
+    "url": "/Test/static/media/Spain.svg.d3fac95b.png"
+  },
+  {
+    "revision": "69547e25f5fe58a8b78777dc82725e04",
+    "url": "/Test/static/media/Switzerland.69547e25.svg"
+  },
+  {
+    "revision": "53d7b7606f296f0ec8dd9b4b5cb6e5ae",
+    "url": "/Test/static/media/Tunisia.svg.53d7b760.png"
+  },
+  {
+    "revision": "85ff41a68431ca8f3e569304ef6ddc77",
+    "url": "/Test/static/media/Uganda.svg.85ff41a6.png"
+  },
+  {
+    "revision": "e6d031f41d8335537c32f94abd97c974",
+    "url": "/Test/static/media/Vanuatu.svg.e6d031f4.png"
+  },
+  {
+    "revision": "ae95f9318e6ded5570d0550fff4993d2",
+    "url": "/Test/static/media/Vatican.svg.ae95f931.png"
+  },
+  {
+    "revision": "a7aa1c88fbadab1c2c629fc67e396d2a",
+    "url": "/Test/static/media/Wales.svg.a7aa1c88.png"
+  },
+  {
+    "revision": "412915369f896b6c5fa3c7652137abc5",
+    "url": "/Test/static/media/avatar.41291536.jpg"
+  },
+  {
+    "revision": "4764fd6748c663e2a2f2c783a7fcc9c7",
+    "url": "/Test/static/media/dollar-logo-png.4764fd67.jpg"
+  },
+  {
+    "revision": "f0476a92cb218fdaeae95a2b51e52135",
+    "url": "/Test/static/media/koronavirus.f0476a92.jpg"
+  },
+  {
+    "revision": "5a0ff6cd6bb507f1f020a5e251c076b9",
+    "url": "/Test/static/media/logo.5a0ff6cd.png"
+  },
+  {
+    "revision": "62ff73a96d0744e78e50eb26755f580a",
+    "url": "/Test/static/media/messages.62ff73a9.jpg"
+  },
+  {
+    "revision": "8046b86552da5a2f48c905aaf052211c",
+    "url": "/Test/static/media/music.8046b865.png"
+  },
+  {
+    "revision": "178ac4754fd9fd24adbe17589f6dbfdf",
+    "url": "/Test/static/media/newkaledonia.svg.178ac475.png"
+  },
+  {
+    "revision": "292ce4ee949a306c504236d86d30241c",
+    "url": "/Test/static/media/profileimg.292ce4ee.jpg"
+  },
+  {
+    "revision": "17612250e6166377bdee6bc91ff40b72",
+    "url": "/Test/static/media/russia.17612250.svg"
+  },
+  {
+    "revision": "1aeecd60c245f84d7db8bd634f0e870b",
+    "url": "/Test/static/media/ukraine.1aeecd60.svg"
+  },
+  {
+    "revision": "aefba3fef673e14305b112b0e82805fe",
+    "url": "/Test/static/media/usa.aefba3fe.svg"
+  }
+]);
